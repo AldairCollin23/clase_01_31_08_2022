3099f7a81bb90945e5f7790ea22d1b6444fb2d0f
aldaircollin23 <juanitoquispe635@gmail.com>
2022-08-31 20:42:15 -0500

    first commit

--- src/java/modelo/Datos.java
+++ src/java/modelo/Datos.java
@@ -5,11 +5,44 @@
  */
 
 package modelo;
-
+import java.sql.*;
 /**
  *
  * @author ILP
  */
 public class Datos {
+    
+    Connection x;
+    
+    PreparedStatement y;
+    
+    
+   public Datos(){
+       try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            x=DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/prueba","root","root"
+            );
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+       
+   }
+   
+   public int registra(String w, String z){
+       int m=0;
+       try {
+           y=x.prepareStatement("insert into cliente values(?,?);");
+           y.setString(1, w);
+           y.setString(2, z);
+           m=y.executeUpdate();
+       } catch (Exception e) {
+       }
+       
+       return m;
+   }
     
 }
